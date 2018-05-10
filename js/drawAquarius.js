25f2e4eaf3a6445ed40cdb8f3230cacf0e51c70b
Aaron Geller <ageller@williamson.local>
2018-05-10 16:06:51 -0500

    added asteroid model from Patrick McPike

--- js/drawAquarius.js
+++ js/drawAquarius.js
@@ -1,41 +1,41 @@
 
 function clearAquarius(){
 
-        MovingAquarius.remove(MovingAquariusMesh);
-        scene.remove(MovingAquarius);
+	MovingAquarius.remove(MovingAquariusMesh);
+	scene.remove(MovingAquarius);
 }
 
 
 //get position of Aquarius
 function createAquariusOrbit(semi, ecc, inc, lan, ap, tperi, period, Ntheta = 10.){
 	var JDtoday = JD0 + (params.Year - 1990.);
-        var tdiff = JDtoday - tperi;
-        var phase = (tdiff % period)/period;
-        
-        var i,j;
-        var b = [-1.*inc, lan, ap];
-        var c = [];
-        var s = [];
-        for (i=0; i<3; i++){
-                c.push(Math.cos(b[i]));
-                s.push(Math.sin(b[i]));
-        }       
-        semi = semi;
-        var P = [];
-        P.push(-1.*c[2]*c[1] + s[2]*c[0]*s[1]);
-        P.push(-1.*c[2]*s[1] - s[2]*c[0]*c[1]);
-        P.push(-1.*s[2]*s[0]); 
-        var Q = [];
-        Q.push(s[2]*c[1] + c[2]*c[0]*s[1]);
-        Q.push(s[2]*s[1] - c[2]*c[0]*c[1]);
-        Q.push(-1.*s[0]*c[2]);
-        
-        var dTheta = 2.*Math.PI / Ntheta;
-        
-        var geometry = new THREE.Geometry();
-        var pos;
-        
-        var E = 0.0;
+	var tdiff = JDtoday - tperi;
+	var phase = (tdiff % period)/period;
+	
+	var i,j;
+	var b = [-1.*inc, lan, ap];
+	var c = [];
+	var s = [];
+	for (i=0; i<3; i++){
+		c.push(Math.cos(b[i]));
+		s.push(Math.sin(b[i]));
+	}       
+	semi = semi;
+	var P = [];
+	P.push(-1.*c[2]*c[1] + s[2]*c[0]*s[1]);
+	P.push(-1.*c[2]*s[1] - s[2]*c[0]*c[1]);
+	P.push(-1.*s[2]*s[0]); 
+	var Q = [];
+	Q.push(s[2]*c[1] + c[2]*c[0]*s[1]);
+	Q.push(s[2]*s[1] - c[2]*c[0]*c[1]);
+	Q.push(-1.*s[0]*c[2]);
+	
+	var dTheta = 2.*Math.PI / Ntheta;
+	
+	var geometry = new THREE.Geometry();
+	var pos;
+	
+	var E = 0.0;
 	i=0;
 	E = (i*dTheta + 2.*phase*Math.PI) % (2.*Math.PI);
 	pos = []
@@ -49,41 +49,64 @@ function makeAquarius( geo, tperi, day, radius, rotation = null) {
 
 	var rotPeriodAquarius = day;
 	var JDtoday = JD0 + (params.Year - 1990.);
-        var tdiff = JDtoday - tperi;
-        var phaseAquarius = (tdiff % rotPeriodAquarius)/rotPeriodAquarius;
+	var tdiff = JDtoday - tperi;
+	var phaseAquarius = (tdiff % rotPeriodAquarius)/rotPeriodAquarius;
 
 	//make slightly bigger so can actually see it
 	var AquariusRad = radius*10000.;
 	//rescale the mesh after creating the sphere.  Otherwise, the sphere will not be drawn correctly at this small size
-        var sc = params.earthRad;
+	var sc = params.earthRad;
 
-        var geometry = new THREE.SphereGeometry(AquariusRad,32,32);
+	var geometry = new THREE.SphereGeometry(AquariusRad,32,32);
 	var AquariusMaterial = new THREE.MeshPhongMaterial( {
-                map: AquariusTex,
-        } );
+		map: AquariusTex,
+	} );
 
-        var mesh = new THREE.Mesh( geometry, AquariusMaterial );
-        if (rotation != null){
-                mesh.rotation.x = THREE.Math.degToRad(90.); // orient map
-		mesh.rotation.y = (2.*phaseAquarius*Math.PI) % (2.*Math.PI); //rotate Aquarius around axis
-                mesh.rotation.z = THREE.Math.degToRad(0.); 
-        }
-        mesh.position.set(geo[0],geo[1],geo[2]);
-	mesh.scale.set(sc, sc, sc);
-	MovingAquariusMesh = mesh;
+	//from https://github.com/mrdoob/three.js/blob/master/examples/webgl_loader_obj.html
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+		console.log( item, loaded, total );
+	};
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+	var onError = function ( xhr ) {
+	};	
+	var textureLoader = new THREE.TextureLoader( manager );
+	var texture = textureLoader.load( 'models/AsteroidCentered/maps/Comet_F_Diffuse03.png' );	
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( 'models/AsteroidCentered/AsteroidQuads.obj', function ( object ) {
+		object.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {
+				child.material.map = texture;
+			}
+		} );
 
+		object.position.set(geo[0],geo[1],geo[2]);
 
-	MovingAquarius = new THREE.Group(); // group Aquarius mesh, then orient orbit 
-	if (rotation != null){
-                MovingAquarius.rotation.x = rotation.x;
-                MovingAquarius.rotation.y = rotation.y;
-                MovingAquarius.rotation.z = rotation.z;
-        }
-	MovingAquarius.add(MovingAquariusMesh);
-	scene.add(MovingAquarius);
+		object.scale.set(sc, sc, sc);
+		
+		MovingAquariusMesh = object;
 
-        scene.updateMatrixWorld(true);
-        params.AquariusPos.setFromMatrixPosition( MovingAquariusMesh.matrixWorld );
+		MovingAquarius = new THREE.Group(); // group Aquarius mesh, then orient orbit 
+		if (rotation != null){
+			MovingAquarius.rotation.x = rotation.x;
+			MovingAquarius.rotation.y = rotation.y;
+			MovingAquarius.rotation.z = rotation.z;
+		}
+		MovingAquarius.add(MovingAquariusMesh);
+		scene.add(MovingAquarius);
+
+		scene.updateMatrixWorld(true);
+		params.AquariusPos.setFromMatrixPosition( MovingAquariusMesh.matrixWorld );
+
+	}, onProgress, onError );
+
+				
+
 }
 
 
